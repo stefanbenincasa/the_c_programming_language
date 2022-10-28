c22ae2a18d4c7ceb7f1664d15bc371b3b7bec783
Stefan Benincasa <stefan.benin@yahoo.com>
2022-10-28 20:38:35 +1100

    completion of fold.c

--- fold.c
+++ fold.c
@@ -1,33 +1,58 @@
 #include <stdio.h>
 
 #define MAXLINE 100
-#define n_column_limit 4 
+#define COLUMN_LIMIT 4 
 
 // Track column count
 // Column count reached and non-blank read, triggers 'line-break'
 
 int get_line(char line[], int limit);
+void folded_print(char line[]);
 
 int main() {
 	char line[MAXLINE];
 	int	length; 
 
 	while((length = get_line(line, MAXLINE)) > 0) {
+		folded_print(line);	
 	}
+
+	printf("\nEND\n");
 
 	return 0;
 }
 
-int get_line(char line[], int limit) {
-	int i, c, prev_char = 0, length = 0, column_count = 0;
-	int first_word = 1;
+void folded_print(char line[]) {
+	int i, j, c, prev_char = 0, length = 0, column_count = 0;
 
-	for(i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-		if(c != ' ') {
-			if(prev_char == 0 || prev_char == ' ') column_count++;
+	printf("\nFolded Lines Column Output:\n");
+	for(i = 0; line[i] != '\0'; ++i) {
+
+		if(line[i] != ' ')  {
+			if(prev_char == 0 || prev_char == ' ') {
+				column_count++;
+			}
+		}		
+
+		if(column_count <= COLUMN_LIMIT) {
+			printf("%c", line[i]);
+		}
+		else {
+			printf("\n%c", line[i]);
+			column_count = 0;
 		}
 
-		prev_char = c;
+		prev_char = line[i];
+	}
+
+	printf("\n"); // For tidy output
+}
+
+int get_line(char line[], int limit) {
+	int i, c, length = 0;
+
+	for(i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+		line[i] = c;
 	}
 
 	if(c == '\n') {
@@ -38,7 +63,6 @@ int get_line(char line[], int limit) {
 	length = i;
 	line[i] = '\0';
 
-	printf("Column Count: %d\n", column_count);
 	return length;
 }
 
